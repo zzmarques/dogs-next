e298fe8fdd28cff9b3c27868fb7d9b12178b5607
zzmarques <walacemarqs@gmail.com>
2025-11-06 08:33:49 -0300

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'Dogs Next',
+  description: 'Rede social para cachorros',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
